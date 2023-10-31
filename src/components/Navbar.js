8288b990b7f8a62ddecbbd20206c15f4edbcc163
toyman640 <falakoadebayor@gmail.com>
2023-10-31 19:27:27 +0100

    Fix linters errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const NavBar = () => {
-
-  return (
-    <div className="Navigation">
-      <h2>Logo</h2>
-      <ul className="NavLinks">
-        <li>Home</li>
-        <li>Shops</li>
-        <li>Register</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  );
-};
+const NavBar = () => (
+  <div className="Navigation">
+    <h2>Logo</h2>
+    <ul className="NavLinks">
+      <li>Home</li>
+      <li>Shops</li>
+      <li>Register</li>
+      <li>Cart</li>
+    </ul>
+  </div>
+);
 
 export default NavBar;
